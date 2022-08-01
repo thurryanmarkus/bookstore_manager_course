5981737d6ce83f1e2d59c056b544278f7f282b57
Thurryan Markus <thurryan.markus@hotmail.com>
2022-08-01 16:50:17 -0300

    Refactores functionality for book creation

---
+++ src/main/java/com/rodrigopeleias/bookstoremanager/controller/BookController.java
@@ -0,0 +1,28 @@
+package com.rodrigopeleias.bookstoremanager.controller;
+
+import com.rodrigopeleias.bookstoremanager.dto.MessageResponseDTO;
+import com.rodrigopeleias.bookstoremanager.entity.Book;
+import com.rodrigopeleias.bookstoremanager.repository.BookRepository;
+import com.rodrigopeleias.bookstoremanager.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/books")
+public class BookController {
+
+    private BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PostMapping
+    public MessageResponseDTO create(@RequestBody Book book){
+        return bookService.create(book);
+    }
+}
